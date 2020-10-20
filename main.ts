41aa615f67f97107cbc9d8b73bd1a536390b3c69
nathan0601 <71689307+nathan0601@users.noreply.github.com>
2020-10-20 15:32:51 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,23 @@
 basic.forever(function () {
-    for (let y = 0; y <= 4; y++) {
-        for (let x = 0; x <= 4; x++) {
-            led.plot(x, y)
-            basic.pause(100)
-        }
+    for (let x = 0; x <= 4; x++) {
+        led.plot(Math.abs(x - 4), 4)
+        basic.pause(100)
     }
-    for (let y = 0; y <= 4; y++) {
-        for (let x = 0; x <= 4; x++) {
-            led.unplot(x, y)
-            basic.pause(100)
-        }
+    for (let x = 0; x <= 4; x++) {
+        led.plot(x, 3)
+        basic.pause(100)
+    }
+    for (let x = 0; x <= 4; x++) {
+        led.plot(Math.abs(x - 4), 2)
+        basic.pause(100)
+    }
+    for (let x = 0; x <= 4; x++) {
+        led.plot(x, 1)
+        basic.pause(100)
+    }
+    for (let x = 0; x <= 4; x++) {
+        led.plot(Math.abs(x - 4), 4)
+        basic.pause(100)
     }
     basic.clearScreen()
 })
