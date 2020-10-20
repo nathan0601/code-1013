c6e25a5b23398875b3c47a44a7a4a4c2787c7f55
nathan0601 <71689307+nathan0601@users.noreply.github.com>
2020-10-20 14:27:35 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,5 +5,11 @@ basic.forever(function () {
             basic.pause(100)
         }
     }
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.unplot(x, y)
+            basic.pause(100)
+        }
+    }
     basic.clearScreen()
 })
